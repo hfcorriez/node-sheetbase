3db56169de881e93d1d99c5eb6c9a423d1a0d826
hfcorriez <hfcorriez@gmail.com>
2020-02-11 17:52:45 +0800

    export Spreadsheet

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const Main =  require('./lib/sheetbase')
 
 Main.Drive = require('./lib/drive')
 Main.Cache = require('./lib/cache')
+Main.Spreadsheet = require('./lib/spreadsheet')
 
 module.exports = Main
